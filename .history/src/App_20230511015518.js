46292c84f3190f3a2d90bc7ecc2744e78ed091a8
Yoo Seungmin <dnjfht@gmail.com>
2023-05-11 01:57:05 +0900

    Dark Mode - Context API

--- .history/src/App_20230511015518.js
+++ .history/src/App_20230511015518.js
@@ -7,8 +7,6 @@ import { DarkModeProvider } from "./DarkModeContext";
 export default function App() {
   return (
     <DarkModeProvider>
-      {/* Context를 가져다 사용하려면 이렇게 우산을 씌워주면 된다. */}
-      {/* 이렇게 해주면 PRovider 아래에 있는 자식 component들이 모두 darkMode, toggleDarkMode를 가져다 쓸 수 있다 */}
       <div style={{ width: "100%", height: "100vh" }}>
         <Header />
         <Main />
---
+++ .history/src/components/Main_20230511015501.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Profile2 from "./Profile2";
+import Products2 from "./Products2";
+
+export default function Main() {
+  return (
+    <div>
+      Main
+      <Profile2 />
+      <Products2 />
+    </div>
+  );
+}
