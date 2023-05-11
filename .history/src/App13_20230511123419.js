6fc4fb5a7080ed1e6f02eb599f160562ee8b902b
Yoo Seungmin <dnjfht@gmail.com>
2023-05-11 12:35:38 +0900

    로딩, 에러 상태 추가

---
+++ .history/src/App13_20230511123419.js
@@ -0,0 +1,124 @@
+import React, { memo, useCallback, useMemo, useState } from "react";
+
+export default function App13() {
+  const [person, setPerson] = useState({
+    name: "승민",
+    title: "개발자",
+    mentors: [
+      {
+        name: "동동주",
+        title: "시니어개발자",
+      },
+      {
+        name: "제임스",
+        title: "시니어개발자",
+      },
+      {
+        name: "티나",
+        title: "시니어개발자",
+      },
+    ],
+  });
+
+  const ClickMentorNameChange = useCallback(() => {
+    const prev = prompt("누구의 이름을 바꾸고 싶은가요?");
+    const current = prompt("이름을 무엇으로 바꾸고 싶은가요?");
+
+    setPerson((person) => ({
+      ...person,
+      mentors: person.mentors.map((mentor) => {
+        if (mentor.name === prev) {
+          return { ...mentor, name: current };
+        }
+        return mentor;
+      }),
+    }));
+  }, []);
+
+  const ClickAddMentor = useCallback(() => {
+    const name = prompt("추가할 멘토의 이름은 무엇입니까?");
+    const title = prompt("추가할 멘토의 직함은 무엇입니까?");
+
+    setPerson((person) => ({
+      ...person,
+      mentors: [...person.mentors, { name, title }],
+    }));
+  }, []);
+
+  const ClickDeleteMentor = useCallback(() => {
+    const name = prompt("삭제할 멘토의 이름은 무엇입니까?");
+
+    setPerson((person) => ({
+      ...person,
+      mentors: person.mentors.filter((mentor) => {
+        return mentor.name !== name;
+      }),
+    }));
+  }, []);
+
+  // useCallback으로 감싸주면
+  // ClickMentorNameChange, ClickAddMentor, ClickDeleteMentor 세 함수가 동일한 callback 객체를 사용해서
+  // 이제 다시 무언가 업데이트 될 때 컴포넌트에서 동일한 객체를 사용하겠지만
+  // 계속 새롭게 할당된 text가 만들어질 거임.
+  // 그리고 props를 전달할 되면 매번 컴포넌트를 호출할 때마다 새로운 props라는 객체가 생성이 됨.
+  return (
+    <div>
+      <h1>
+        {person.name}은 {person.title}
+      </h1>
+      <p>{person.name}의 멘토는 :</p>
+      <ul>
+        {person.mentors.map((mentor, index) => {
+          return (
+            <li key={index}>
+              {mentor.name} ({mentor.title})
+            </li>
+          );
+        })}
+      </ul>
+      <Button text="멘토 이름 바꾸기" onClick={ClickMentorNameChange} />
+      <Button text="멘토 추가하기" onClick={ClickAddMentor} />
+      <Button text="멘토 삭제하기" onClick={ClickDeleteMentor} />
+    </div>
+  );
+}
+
+// 아무리 똑같은 값을 props로 전달을 해도 매번 새로운 객체가 만들어짐.
+// 값이 전달될 때, 매번 새로운 객체가 만들어지더라도 안에 들어있는 값이 동일하다면 다시 리렌더링 하지마!라고 memo를 해줄 거임.
+
+// App component가 아무리 변경이 되어도
+// 콜백힘수를 useCallback으로 감싸줬고, Button 컴포넌트의 text가 변경되지 않는 이상
+// 항상 동일한 컴포넌트를 기억하고 있기 때문에 리렌더링이 발생 X
+
+const Button = memo(({ text, onClick }) => {
+  console.log("Button", text, "re-rendering !!");
+  const result = useMemo(() => calculateSomething(), []);
+  // component 안에 뭔가 무거운 일을 하는데 첫 렌더링 때만 계산하면 된다면
+  // useEffect를 사용하면 된다.
+
+  // 또는 useMemo를 사용할 수 있음. => 이거 기억(캐쉬)해놔!
+  // useMemo가 호출될 때 딱 한 번만 함수를 호출해줄 거임.
+
+  // 이것도 싫다면 useCallback을 사용하여 함수가 딱 한 번만 실행되도록 하겠음.
+
+  return (
+    <button
+      onClick={onClick}
+      style={{
+        backgroundColor: "black",
+        color: "white",
+        borderRadius: 20,
+        margin: "0.4rem",
+      }}
+    >
+      {`${text} ${result}`}
+    </button>
+  );
+});
+
+function calculateSomething() {
+  for (let i = 0; i < 10000; i++) {
+    console.log("💖💖💖");
+  }
+  return 10;
+}
