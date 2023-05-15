4cee5ed3e30867dd568494333fcd7ebee3fcfb36
Yoo Seungmin <dnjfht@gmail.com>
2023-05-15 14:11:45 +0900

    tailwindcss

--- .history/src/App_20230515133903.js
+++ .history/src/App_20230515133903.js
@@ -1,15 +1,12 @@
 import React from "react";
 import Button1 from "./components/Button1";
 import Button2 from "./components/Button2";
-import StyledComponents from "./StyledComponents";
 
 export default function App() {
   return (
     <div>
       <Button1 />
       <Button2 />
-
-      <StyledComponents />
     </div>
   );
 }
---
+++ src/TailwindComponents.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function TailwindComponents() {
+  return <div></div>;
+}
